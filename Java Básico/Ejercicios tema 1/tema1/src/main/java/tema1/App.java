3e97f12c24fd99005df7523219d42ce06d01c769
Jhony Castellanos <jhonycastellanos12@gmail.com>
2023-03-16 20:28:34 -0500

    Java Básico Tema 1

---
+++ Java Básico/Ejercicios tema 1/tema1/src/main/java/tema1/App.java
@@ -0,0 +1,22 @@
+package tema1;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App {
+    public static void main(String[] args) {
+        int entero = 123;
+        long enteroGrande = 123123;
+        double real = 1.1;
+        boolean logico = true;
+        String texto = "hola";
+
+        System.out.println(entero);
+        System.out.println(enteroGrande);
+        System.out.println(real);
+        System.out.println(logico);
+        System.out.println(texto);
+    }
+}
